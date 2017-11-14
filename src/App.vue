0fd6419740d1d29cfb1ec1d76a0569e2b81dfa0f
Pat Richeal <patrickricheal@gmail.com>
2017-11-14 00:04:57 -0500

    Hooked up clips to new streamable-api and added pagination

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="app">
-    <Clip shortCode="vd4go"/>
+    <div class="clips">
+      <Clip v-for="(value, index) in clips" :key="index" :clip="value" :number="(total - index) - ((page - 1) * count)"/>
+    </div>
+    <div class="footer">
+      <span class="page-number" v-for="n in totalPages" :key="n" v-on:click="goToPage(n)" v-bind:class="{active: n == page}">{{ n }}</span>
+    </div>
   </div>
 </template>
 
@@ -10,16 +15,47 @@ import axios from 'axios'
 
 export default {
   name: 'App',
-  created: function () {
-    axios.get('https://ajax.streamable.com/videos?sort=date_added&sortd=DESC&count=12&page=1', {headers: {'set-cookie': 'test=test'}}).then(response => {
-      console.log(response)
-      this.clips = response.data.html
-    })
-  },
   data () {
     return {
-      clips: null
+      clips: [],
+      sort: 'date_added',
+      sortd: 'DESC',
+      count: 12,
+      page: 1,
+      total: 0,
+      totalPages: 0,
+      loading: true
     }
+  },
+  methods: {
+    downloadClips: function () {
+      let params = {
+        sort: this.sort,
+        sortd: this.sortd,
+        count: this.count,
+        page: this.page
+      }
+      axios.get('http://localhost:3000', {params: params})
+      .then(response => {
+        this.clips = response.data.videos
+        this.total = response.data.total
+        this.totalPages = Math.ceil(this.total / this.count)
+        console.log(this.clips)
+      }).catch(error => {
+        alert(error)
+      })
+    },
+    goToPage: function (page) {
+      this.page = page
+      this.downloadClips()
+    },
+    nextPage: function () {
+      this.page++
+      this.downloadClips()
+    }
+  },
+  created: function () {
+    this.downloadClips()
   },
   components: {
     Clip
@@ -28,9 +64,39 @@ export default {
 </script>
 
 <style>
-div.app {
+div.clips {
   display: flex;
   flex-wrap: wrap;
+  font-size: 12px;
   justify-content: center;
+  margin: 40px 0 120px 0;
+  overflow: auto;
 }
+
+div.footer {
+  background-color: #f1f1f1;
+  border-top: 1px solid #dedede;
+  bottom: 0;
+  padding: 30px 0;
+  position: fixed;
+  text-align: center;
+  width: 100%;
+  z-index: 2;
+}
+
+span.page-number {
+  cursor: pointer;
+  margin: 0 4px;
+  padding: 3px;
+}
+
+span.page-number:hover {
+  color: gray;
+}
+
+span.page-number.active {
+  color: gray;
+  font-weight: 700;
+}
+
 </style>
--- src/components/Clip.vue
+++ src/components/Clip.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="clip">
-    <div v-html="embed"></div>
+    <span class="plays">{{ clip.plays }} views</span>
+    <div class="embed" v-html="embed"></div>
     <div class="info">
-      <div class="title">{{ title }}</div>
-      <div class="date">{{ date }}</div>
+      <div class="title">
+        {{ number }}. <a :href="clip.url">{{ clip.title }}</a>
+      </div>
+      <div class="date">{{ formatDate(clip.date_added) }}</div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import moment from 'moment'
 
 export default {
   name: 'Clip',
-  props: ['shortCode', 'title', 'plays', 'date'],
+  props: ['clip', 'number'],
   created: function () {
-    axios.get('https://api.streamable.com/oembed.json?url=https://streamable.com/' + this.shortCode).then(response => {
-      this.embed = response.data.html
-    })
+    this.downloadEmbed()
+  },
+  updated: function () {
+    this.downloadEmbed()
   },
   data () {
     return {
       embed: null
+    }
+  },
+  methods: {
+    downloadEmbed: function () {
+      axios.get('https://api.streamable.com/oembed.json?url=https://streamable.com/' + this.clip.shortcode).then(response => {
+        this.embed = response.data.html
+      })
+    },
+    formatDate: function (unixTimestamp) {
+      return moment.unix(unixTimestamp).format('D MMMM YYYY')
     }
   }
 }
@@ -31,9 +46,24 @@ export default {
 <style scoped>
 div.clip {
   border-radius: 2px;
-  box-shadow: 0 1px 5px rgba(16,22,26,.2);
+  box-shadow: 0 1px 5px rgba(16, 22, 26, 0.2);
   margin: 10px;
+  position: relative;
   width: 400px;
+}
+
+div.embed {
+  background-color: black;
+}
+
+span.plays {
+  color: white;
+  font-size: 12px;
+  position: absolute;
+  right: 5px;
+  text-shadow: 0 0 2px black;
+  top: 3px;
+  z-index: 1;
 }
 
 div.info {
