23bb99213cb1614ce5c7f45f623218d29e9e4740
Pat Richeal <patrickricheal@gmail.com>
2017-11-18 19:48:18 -0500

    Added http:// infront of URL

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,7 @@ export default {
         count: this.count,
         page: this.page
       }
-      axios.get('ec2-34-203-215-132.compute-1.amazonaws.com', {params: params})
+      axios.get('http://ec2-34-203-215-132.compute-1.amazonaws.com', {params: params})
       .then(response => {
         this.clips = response.data.videos
         this.total = response.data.total
